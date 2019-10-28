14fc3463185b275e08e46ceaaed751729b0b0bd2
Teemitze <Teemitze@gmail.com>
2019-10-28 20:29:47 +0300

    Catch main() exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import logger.Logs;
+
+import java.util.logging.Level;
+
 public class Main {
     static final String HH = "https://hh.ru";
+
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", Configuration.GECKO_DRIVER);
-        new Browser();
+        try {
+            System.setProperty("webdriver.gecko.driver", Configuration.GECKO_DRIVER);
+            new Browser();
+        } catch (Exception e) {
+            Logs.infoLog.log(Level.SEVERE, "Exception!", e);
+        }
     }
 }
